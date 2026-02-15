0083cc1fc4aaf562f6d151784301995aff4234cd
Anooneee <72642342+Anooneee@users.noreply.github.com>
2026-02-15 09:11:08 -0700

    j

--- book_resources/BookTypes.gd
+++ book_resources/BookTypes.gd
@@ -2,10 +2,10 @@ extends Resource
 class_name BookTypes
 
 enum Types {
-    BLUE,
-    RED,
-    YELLOW,
-    GREEN
+	BLUE,
+	RED,
+	YELLOW,
+	GREEN
 }
 @export var currentType:Types
 @export var piece_num := 3
@@ -13,18 +13,18 @@ enum Types {
 var max_num_of_pieces = 1
 
 func _ready():
-    set_type()
-    piece_num = clamp(piece_num,1,max_num_of_pieces)
-    pass
+	set_type()
+	piece_num = clamp(piece_num,1,max_num_of_pieces)
+	pass
 
 func set_type():
-    match currentType:
-        Types.BLUE:
-            max_num_of_pieces = 3
-        Types.RED:
-            max_num_of_pieces = 2
-        Types.YELLOW:
-            max_num_of_pieces = 1
-        Types.GREEN:
-            max_num_of_pieces = 2
-    pass
+	match currentType:
+		Types.BLUE:
+			max_num_of_pieces = 3
+		Types.RED:
+			max_num_of_pieces = 2
+		Types.YELLOW:
+			max_num_of_pieces = 1
+		Types.GREEN:
+			max_num_of_pieces = 2
+	pass
