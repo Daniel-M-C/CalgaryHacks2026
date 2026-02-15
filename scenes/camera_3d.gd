4a185d93896afa054d34171ff29ada79eb698955
CplCanuck <blueguylastname@gmail.com>
2026-02-15 04:13:09 -0700

    Update to Initial Follow Camera

---
+++ scenes/camera_3d.gd
@@ -0,0 +1,17 @@
+extends Camera3D
+
+@onready var look_target: Node3D = $"../../Player"
+@onready var target_direction: Node3D = $"../TargetDirection"
+@export var look_speed := 5
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+    pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+    target_direction.look_at(look_target.position)
+    self.rotation = self.rotation.lerp(target_direction.rotation, look_speed * delta)
+    pass
